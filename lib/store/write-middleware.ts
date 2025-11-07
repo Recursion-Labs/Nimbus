319167acbdc38df23a84004fd02d02b9a8cbb30a
Ayush <ayushsarkar357@gmail.com>
2025-11-08 00:08:11 +0530

    feat: added write-middleware.ts

---
+++ lib/store/write-middleware.ts
@@ -0,0 +1,19 @@
+import type {Dispatch, Middleware} from 'redux';
+
+import type {NimbusActions, NimbusState} from '../../typings/nimbus';
+import terms from '../terms';
+
+// the only side effect we perform from middleware
+// is to write to the react term instance directly
+// to avoid a performance hit
+const writeMiddleware: Middleware<{}, NimbusState, Dispatch<NimbusActions>> = () => (next) => (action: NimbusActions) => {
+  if (action.type === 'SESSION_PTY_DATA') {
+    const term = terms[action.uid];
+    if (term) {
+      term.term.write(action.data);
+    }
+  }
+  next(action);
+};
+
+export default writeMiddleware;

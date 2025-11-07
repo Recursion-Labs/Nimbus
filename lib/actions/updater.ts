b612067f47ee06c993eab38d9b821189eaa9b726
Ayush <ayushsarkar357@gmail.com>
2025-11-08 00:07:52 +0530

    feat: added updater.ts

---
+++ lib/actions/updater.ts
@@ -0,0 +1,22 @@
+import {UPDATE_INSTALL, UPDATE_AVAILABLE} from '../../typings/constants/updater';
+import type {NimbusActions} from '../../typings/nimbus';
+import rpc from '../rpc';
+
+export function installUpdate(): NimbusActions {
+  return {
+    type: UPDATE_INSTALL,
+    effect: () => {
+      rpc.emit('quit and install');
+    }
+  };
+}
+
+export function updateAvailable(version: string, notes: string, releaseUrl: string, canInstall: boolean): NimbusActions {
+  return {
+    type: UPDATE_AVAILABLE,
+    version,
+    notes,
+    releaseUrl,
+    canInstall
+  };
+}
